87f03d10c894991254448dfa4c9f07aaf85ec817
Shall's Macbook <shalljaiden@d051-173.jaist.ac.jp>
2025-06-28 15:10:32 +0900

    add config

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -4,22 +4,27 @@ import (
 	"database/sql"
 	"log"
 	"os"
+	"simplebank/util"
 	"testing"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgres://root:qwe123@localhost:5432/simple_bank?sslmode=disable"
-)
+// const (
+// 	dbDriver = "postgres"
+// 	dbSource = "postgres://root:qwe123@localhost:5432/simple_bank?sslmode=disable"
+// )
 
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../../.")
+	if err != err {
+		log.Fatal("connot load config:", err)
+	}
+
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
