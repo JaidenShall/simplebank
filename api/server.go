0ec938baef1db6fac66288c64b75f5e7e456bb08
Shall's Macbook <shalljaiden@d051-173.jaist.ac.jp>
2025-07-04 22:00:23 +0900

    add validator

--- api/server.go
+++ api/server.go
@@ -2,8 +2,10 @@ package api
 
 import (
 	db "github.com/JaidenShall/simplebank/db/sqlc"
+	"github.com/go-playground/validator/v10"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
 )
 
 // Server serves HTTP requests for our banking service.
@@ -16,6 +18,10 @@ type Server struct {
 func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
+
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
 
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
